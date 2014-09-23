a7be10b7bf5e6d1aaa96724692f1cf6058123e5e
Ravi Hasija <ravi.hasija@gmail.com>
2014-09-22 21:34:03 -0500

    Reformatted the code.

--- grails-app/atmosphere/org/repose/ChatMeteorHandler.groovy
+++ grails-app/atmosphere/org/repose/ChatMeteorHandler.groovy
@@ -1,53 +1,49 @@
 package org.repose
 
-import org.atmosphere.cpr.AtmosphereResourceEvent
-import org.atmosphere.cpr.AtmosphereResourceEventListenerAdapter
-import org.atmosphere.cpr.Broadcaster
-import org.atmosphere.cpr.DefaultBroadcaster
-import org.atmosphere.cpr.Meteor
+import grails.util.Holders
+import org.atmosphere.cpr.*
+import org.atmosphere.websocket.WebSocketEventListenerAdapter
 
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-import org.atmosphere.websocket.WebSocketEventListenerAdapter
-import grails.util.Holders
 import static org.atmosphere.cpr.AtmosphereResource.TRANSPORT.WEBSOCKET
 
 class ChatMeteorHandler extends HttpServlet {
 
-	def atmosphereMeteor = Holders.applicationContext.getBean("atmosphereMeteor")
-	def atmosphereTestService = Holders.applicationContext.getBean("atmosphereTestService")
+    def atmosphereMeteor = Holders.applicationContext.getBean("atmosphereMeteor")
+    def atmosphereTestService = Holders.applicationContext.getBean("atmosphereTestService")
 
-	@Override
-	void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String mapping = "/atmosphere/chat" + request.getPathInfo()
-		Broadcaster b = atmosphereMeteor.broadcasterFactory.lookup(DefaultBroadcaster.class, mapping, true)
-		Meteor m = Meteor.build(request)
+    @Override
+    void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String mapping = "/atmosphere/chat" + request.getPathInfo()
+        Broadcaster b = atmosphereMeteor.broadcasterFactory.lookup(DefaultBroadcaster.class, mapping, true)
+        Meteor m = Meteor.build(request)
 
-		if (m.transport().equals(WEBSOCKET)) {
-			m.addListener(new WebSocketEventListenerAdapter() {
-				@Override
-				void onDisconnect(AtmosphereResourceEvent event) {
-					atmosphereTestService.sendDisconnectMessage(event, request)
-				}
-			})
-		} else {
-			m.addListener(new AtmosphereResourceEventListenerAdapter() {
-				@Override
-				void onDisconnect(AtmosphereResourceEvent event) {
-					atmosphereTestService.sendDisconnectMessage(event, request)
-				}
-			})
-		}
+        if (m.transport().equals(WEBSOCKET)) {
+            m.addListener(new WebSocketEventListenerAdapter() {
+                @Override
+                void onDisconnect(AtmosphereResourceEvent event) {
+                    atmosphereTestService.sendDisconnectMessage(event, request)
+                }
+            })
+        } else {
+            m.addListener(new AtmosphereResourceEventListenerAdapter() {
+                @Override
+                void onDisconnect(AtmosphereResourceEvent event) {
+                    atmosphereTestService.sendDisconnectMessage(event, request)
+                }
+            })
+        }
 
-		m.setBroadcaster(b)
-	}
+        m.setBroadcaster(b)
+    }
 
-	@Override
-	void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String mapping = "/atmosphere/chat" + request.getPathInfo();
+    @Override
+    void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String mapping = "/atmosphere/chat" + request.getPathInfo();
         LogFileUpdateService logFileUpdateService = Holders.applicationContext.getBean("logFileUpdateService")
         logFileUpdateService.handleLog(mapping)
-	}
+    }
 }
