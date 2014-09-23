f55fe11b524c1a13e4b89c96e8bf2da67b492de8
Ravi Hasija <ravi.hasija@gmail.com>
2014-09-22 21:33:27 -0500

    Things are working way better. Cleaned up some stuff.

--- grails-app/atmosphere/org/repose/ChatMeteorHandler.groovy
+++ grails-app/atmosphere/org/repose/ChatMeteorHandler.groovy
@@ -6,9 +6,6 @@ import org.atmosphere.cpr.Broadcaster
 import org.atmosphere.cpr.DefaultBroadcaster
 import org.atmosphere.cpr.Meteor
 
-import grails.converters.JSON
-import org.repose.TailFileService
-
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -21,7 +18,7 @@ class ChatMeteorHandler extends HttpServlet {
 
 	def atmosphereMeteor = Holders.applicationContext.getBean("atmosphereMeteor")
 	def atmosphereTestService = Holders.applicationContext.getBean("atmosphereTestService")
-	
+
 	@Override
 	void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String mapping = "/atmosphere/chat" + request.getPathInfo()
@@ -50,19 +47,7 @@ class ChatMeteorHandler extends HttpServlet {
 	@Override
 	void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String mapping = "/atmosphere/chat" + request.getPathInfo();
-
-		String message = TailFileService.stopWhenFound("/var/log/repose/current.log", "Repose Ready")
-
-        if (message == null) {
-			atmosphereTestService.recordIncompleteMessage()
-		} else {
-			if (message.toLowerCase().contains("<script")) {
-				atmosphereTestService.recordMaliciousUseWarning(message)
-			} else {
-                atmosphereTestService.recordChat(message)
-				Broadcaster b = atmosphereMeteor.broadcasterFactory.lookup(DefaultBroadcaster.class, mapping)
-                b.broadcast(message)
-			}
-		}
+        LogFileUpdateService logFileUpdateService = Holders.applicationContext.getBean("logFileUpdateService")
+        logFileUpdateService.handleLog(mapping)
 	}
 }
---
+++ grails-app/services/org/repose/LogFileUpdateService.groovy
@@ -0,0 +1,72 @@
+package org.repose
+
+import grails.util.Holders
+import org.atmosphere.cpr.Broadcaster
+import org.atmosphere.cpr.DefaultBroadcaster
+
+class LogFileUpdateService {
+    static final String LOG_FILE_NAME = "/var/log/repose/current.log"
+    def atmosphereMeteor = Holders.applicationContext.getBean("atmosphereMeteor")
+    boolean stop = false
+    static scope = "prototype"
+
+    def stop() {
+        stop = true
+    }
+
+    def tail(Closure c) {
+        def file = new File(LOG_FILE_NAME)
+        def runnable = {
+            int length = file.length()
+            def reader = file.newReader()
+            try {
+                reader.skip(length)
+
+                def line
+                while (!stop) {
+                    line = reader.readLine()
+                    log.debug("Line is empty!")
+                    if (line) {
+                        log.info("Line read from file: $line")
+                        c.call(line)
+                    } else {
+                        Thread.currentThread().sleep(500)
+                    }
+                }
+            }
+            finally {
+                reader?.close()
+            }
+        } as Runnable
+
+        def t = new Thread(runnable)
+        t.start()
+    }
+
+    def handleLog(String mapping) {
+        def message = ""
+        Broadcaster b = atmosphereMeteor.broadcasterFactory.lookup(DefaultBroadcaster.class, mapping)
+
+        tail({ message += " $it" })
+        int i = 20
+        while (1) {
+            if (i == 0 || hasReposeFinishedUpdating(message?.toLowerCase())) {
+                b.broadcast(message ?: "Ending the wait...")
+                break
+            } else if (!message) {
+                b.broadcast("Waiting for file to update for $i seconds...\n")
+            } else {
+                b.broadcast(message)
+            }
+            i--
+            Thread.sleep(1000)
+        }
+        stop()
+        return message
+    }
+
+    def hasReposeFinishedUpdating(String message) {
+        message.contains("repose Ready") ||
+        message.contains("updated file")
+    }
+}
